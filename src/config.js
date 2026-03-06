f165082d32616ff60f74644fe3e679004f093329
Dr-GJP <gjp1148435900@gmail.com>
2026-03-06 16:09:54 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -30,8 +30,8 @@ const config = {
 			"type": "pic",   //"pic":静态壁纸;"video":动态壁纸
 			"datainfo": {
 				"title": "海洋女孩",
-				"preview": "/img/wallpaper/static/海洋女孩/image-pre.webp",
-				"url": "/img/wallpaper/static/海洋女孩/image.png",     //当然，也可填写网络地址或壁纸api，如随机PC壁纸api："url":"https://t.mwm.moe/pc"
+				"preview": "/img/wallpaper/static/1000000873.jpg",
+				"url": "/img/wallpaper/static/1000000873.jpg",     //当然，也可填写网络地址或壁纸api，如随机PC壁纸api："url":"https://t.mwm.moe/pc"
 			},
 		},
 		"mobile": {   //移动端
